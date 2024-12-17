6666e370ecff620eafca1018db3671861b836e2a
NNKtv28 <73384770+NNKTV28@users.noreply.github.com>
2024-12-17 23:50:47 +0100

    settings parsed to JSON config

--- utils/inaraAPI.js
+++ utils/inaraAPI.js
@@ -1,25 +1,28 @@
-const axios = require("axios");
 require("dotenv").config();
+const { appName, version, isBeingDeveloped, commanderName } = require('../config.json');
+const fetch = require("node-fetch");
 
 const INARA_API_URL = "https://inara.cz/inapi/v1/";
 
 async function fetchInaraData(endpoint, data) {
-  try {
-    const response = await axios.post(INARA_API_URL, {
+  const response = await fetch(INARA_API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
       header: {
-        appName: "InaNewsBot",
-        appVersion: "1.0.0",
-        isDeveloped: true,
+        appName: appName,
+        appVersion: version,
+        isBeingDeveloped: isBeingDeveloped,
         APIkey: process.env.INARA_API_KEY,
-        commanderName: "NNKTV28",
+        commanderName: commanderName,
+        commanderFrontierID: ""
       },
-      events: [data],
-    });
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching INARA data:", error.message);
-    return null;
-  }
+      events: [data]
+    })
+  });
+  return response.json();
 }
 
 module.exports = { fetchInaraData };
